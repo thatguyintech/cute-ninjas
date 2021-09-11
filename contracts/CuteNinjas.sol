b59e4ad6e4bacf2f03d32ce555d4407f1c84d7dd
ALBERT <thatguyintech@gmail.com>
2021-09-10 17:54:57 -0700

    test mint logic

--- contracts/CuteNinjas.sol
+++ contracts/CuteNinjas.sol
@@ -10,33 +10,57 @@ contract CuteNinjas is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIds;
-    string private _baseURIex;
+    string private _contractBaseURI;
+    string private _contractURI;
 
-    uint256 private _price = 0.01 ether;
+    uint256 public ninjaPrice = 0.01 ether;
     uint16 public constant MAX_NINJAS = 100;
     uint8 private _totalSupply = 0;
     // string private _baseContractURI;
 
-    constructor(string memory baseURI) ERC721("CuteNinjas", "CNJ") {
-      _baseURIex = baseURI;
+    constructor(string memory bURI, string memory cURI) ERC721("CuteNinjas", "CNJ") {
+      _contractBaseURI = bURI;
+      _contractURI = cURI;
+    }
+
+    // should return something like: 
+    // 
+    // {
+    //   "name": "CuteNinjas",
+    //   "description": "Teleport a cute ninja to defend your kingdom.",
+    //   "image": "https://openseacreatures.io/image.png",
+    //   "external_link": "https://openseacreatures.io",
+    //   "seller_fee_basis_points": 250, # Indicates a 1% seller fee.
+    //   "fee_recipient": "0xcbb517E9321304Dd52ffC9d5970597EF76C35d21" # Where seller fees will be paid to.
+    // }
+    function contractURI() public view returns (string memory) {
+      return _contractURI;
+    }
+
+    function setContractURI(string memory newContractURI) public onlyOwner {
+      _contractURI = newContractURI;
+    }
+
+    function getBaseURI() public view returns (string memory) {
+      return _contractBaseURI;
+    }
+
+    function setBaseURI(string memory newBaseURI) public onlyOwner {
+      _contractBaseURI = newBaseURI;
     }
 
     function totalSupply() view public returns (uint8) {
       return _totalSupply;
     }
-    
-    function priceToMint() view public returns (uint256) {
-      return _price;
-    }
 
     function tokenURI(uint256 tokenId) override view public returns (string memory) {
-      return bytes(_baseURIex).length > 0 ? string(abi.encodePacked(_baseURIex, tokenId.toString())) : "";
+      return bytes(_contractBaseURI).length > 0 ? string(abi.encodePacked(_contractBaseURI, tokenId.toString())) : "";
     }
 
     function mintNFT(uint8 numNinjas) public payable {
         require(_totalSupply + numNinjas <= MAX_NINJAS, "Minting would exceed max allowed Ninjas.");
         require(numNinjas <= 2, "You can mint at most 2 ninjas in one transaction.");
-        require(msg.value >= _price * numNinjas, "Ether value sent is below the price");
+        require(msg.value >= ninjaPrice * numNinjas, "Ether value sent is below the price");
 
         for (uint i=0; i < numNinjas; i++) {
           _tokenIds.increment();
--- test/cute-ninjas-test.js
+++ test/cute-ninjas-test.js
@@ -1,9 +1,41 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const BASE_URI = "ipfs://FAKE_IPFS_CID/";
+const CONTRACT_URI= "ipfs://FAKE_IPFS_CID_FOR_CONTRACT_METADATA";
 const TEST_WALLET = "0x243dc2F47EC5A0693C5c7bD39b31561cCd4B0e97";
 
 describe("CuteNinjas", function () {
-  xit("Should mint a new token", async function () {});
+  let cuteNinjas;
+
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    const CuteNinjas = await ethers.getContractFactory("CuteNinjas");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // for it to be deployed(), which happens once its transaction has been
+    // mined.
+    cuteNinjas = await CuteNinjas.deploy(BASE_URI, CONTRACT_URI);
+  });
+
+  it("Should deploy and set baseURI and contractURI", async function () {
+    expect(await cuteNinjas.getBaseURI()).to.equal(BASE_URI);
+    expect(await cuteNinjas.contractURI()).to.equal(CONTRACT_URI);
+  });
+
+  it("Should mint a new token", async function () {
+    const ninjaPrice = await cuteNinjas.ninjaPrice();
+    const tokenId = await cuteNinjas.totalSupply()+1;
+
+    expect(
+      await cuteNinjas.mintNFT(1, {
+        value: ninjaPrice,
+      })
+    ).to.emit(cuteNinjas, "Transfer")
+    .withArgs(ethers.constants.AddressZero, owner.address, tokenId);
+    expect(await cuteNinjas.balanceOf(owner.address)).to.equal(1);
+  });
   xit("Should return a valid token URI", async function () {});
 });
